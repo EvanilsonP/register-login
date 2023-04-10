cc52474e81dbbed6e1d1ee9dd12d189c42453709
EvanilsonP <pevanilson99@gmail.com>
2023-04-10 11:15:07 -0300

    creating routes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Say hi');
+const PORT = process.env.PORT || 3000;
+const db = require('./database/db-config');
+const cookie = require('cookie-parser');
+app.use('js/', express.static(__dirname + './public/js'));
+app.use('css/', express.static(__dirname + './public/css'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(cookie());
+app.use(express.json());
+db.connect((err) => {
+    if(err) throw err;
+    console.log('Database connected.');
 });
+app.use('/', require('./routes/pages'));
 
 app.listen(PORT, () => console.log(`Up and running on PORT ${PORT}`));
